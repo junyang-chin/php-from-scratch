c4d2245f7548e28a910f8ab9d929e6dd5461cca0
junyang-chin <danny_cjy@hotmail.com>
2023-12-14 02:35:59 +0800

    add psr container

--- app/Core/Container/Container.php
+++ app/Core/Container/Container.php
@@ -1,27 +1,42 @@
 <?php
 
-namespace App\Core;
+namespace App\Core\Container;
 
 use App\Core\Container\Exceptions\BindingNotFoundException;
-use Psr\Container\ContainerInterface;
+use Closure;
+use Psr\Container\ContainerInterface as PsrContainer;
 
-class Container implements ContainerInterface
+class Container implements PsrContainer
 {
-    private static Container $instance;
-
     private $bindings = [];
 
     public function get(string $id)
     {
         if (! isset($this->bindings[$id])) {
-            throw new BindingNotFoundException($id);
+            throw BindingNotFoundException::make($id);
         }
 
-        return $this->bindings[$id]();
+        return $this->bindings[$id];
     }
 
     public function has(string $id): bool
     {
         return isset($this->bindings[$id]);
+    }
+
+    public function bind(string $abstract, Closure|string $concrete): void
+    {
+        $this->bindings[$abstract] = $concrete;
+    }
+
+    public function resolve(string $abstract, array $params = [])
+    {
+        $concrete = $this->get($abstract);
+
+        if (! $concrete instanceof Closure) {
+            return $concrete;
+        }
+
+        return call_user_func_array($concrete, [$this, ...$params]);
     }
 }
